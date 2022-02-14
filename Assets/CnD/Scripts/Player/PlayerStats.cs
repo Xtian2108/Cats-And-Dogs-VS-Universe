f26b0a4994531ce8aa57413a20dd42137f579abe
Xtian2108 <christian_2108@live.com>
2022-02-14 08:52:27 -0500

    Adding a ReadOnly Attribute as Util

--- Assets/CnD/Scripts/Player/PlayerStats.cs
+++ Assets/CnD/Scripts/Player/PlayerStats.cs
@@ -1,14 +1,23 @@
 using CnD.ScriptableObjects;
+using CnD.Scripts.Utilitaries;
 using UnityEngine;
 
 namespace CnD.Player.Core
 {
     public class PlayerStats : MonoBehaviour
     {
-        internal int movementSpeed;
-        internal int health;
-        internal int shield;
-        internal int maxShield;
+        [ReadOnly]
+        public int movementSpeed;
+
+        [ReadOnly]
+        public int health;
+
+        [ReadOnly]
+        public int shield;
+
+        [ReadOnly]
+        public int maxShield;
+
         public void SetStats(SOActorModel soActorModel)
         {
             movementSpeed = soActorModel.movementSpeed;
---
+++ Assets/CnD/Scripts/Utilitaries/ReadOnlyAttribute.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace CnD.Scripts.Utilitaries
+{
+    /// <summary>
+    /// Read Only attribute.
+    /// Attribute is use only to mark ReadOnly properties.
+    /// </summary>
+    public class ReadOnlyAttribute : PropertyAttribute { }
+}
